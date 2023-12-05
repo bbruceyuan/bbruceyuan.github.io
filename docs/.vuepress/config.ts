8e5192e0bc3bf758863b3a2cb1d345382181b883
bbruceyuan <bruceyuan@gmail.com>
2023-12-05 22:22:26 +0800

    add comment

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,4 @@
 import { defineUserConfig } from 'vuepress'
-import { searchPlugin } from "@vuepress/plugin-search";
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 import { sitemapPlugin } from "vuepress-plugin-sitemap2";
 import theme from "./theme";
@@ -7,28 +6,23 @@ import theme from "./theme";
 
 export default defineUserConfig({
     lang: 'zh-CN',
-    title: 'BBruceyuan',
-    description: '欢迎查看 BBruceyuan 的博客',
-    head: [
-      ['meta', { name: 'baidu-site-verification', content: 'code-8mNFhNa5tZ' }],
-      ['meta', { name: '360-site-verification', content: 'b5c713d816b0111fd6e0f0a416d598b3' }],
-      ['meta', { name: 'sogou_site_verification', content: 'UBtsNHFicS' }]
-    ],
+    title: '用代码打点酱油的 bbruceyuan',
+    description: '做了一个播客叫做打点酱油，平常写 Python, 对NLP、计算广告、大模型感兴趣',
+    
+    // 这是我的 一些搜索 相关的配置
+    // head: [
+    //   ['meta', { name: 'baidu-site-verification', content: 'code-8mNFhNa5tZ' }],
+    //   ['meta', { name: '360-site-verification', content: 'b5c713d816b0111fd6e0f0a416d598b3' }],
+    //   ['meta', { name: 'sogou_site_verification', content: 'UBtsNHFicS' }]
+    // ],
     theme,
     plugins: [
-        searchPlugin({
-          // 你的选项
-        }),
         googleAnalyticsPlugin({
           id: 'G-H2HX76V70M',
         }),
+
         sitemapPlugin({
           hostname: "https://bbruceyuan.com",
         }),
     ],
-    markdown: {
-      headers: {
-        level: [1, 2, 3, 4, 5],
-      }
-    }
 })
--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -5,6 +5,7 @@ import sidebar from './sidebar';
 export default hopeTheme({
     darkmode: "toggle",
     logo: "/img/icon.jpg",
+    print: false,
     pure: true,
     // 假如你的文档仓库和项目本身不在一个仓库:
     docsRepo: "bbruceyuan/bbruceyuan.github.io",
@@ -13,17 +14,19 @@ export default hopeTheme({
     // 假如文档放在一个特定的分支下，默认为 'main':
     docsBranch: "main",
     // 默认是 false, 设置为 true 来启用
+    contributors: false,
     
     hotReload: true,
+ 
+    navbar: nav,
     navbarLayout: {
         start: ["Brand"],
         center: [],
-        end: ["Links", "Repo", "Search"]
+        end: ["Links", "Repo", "Search", "Outlook"]
     },
-    navbar: nav,
     sidebar: sidebar,
     blog: {
-        description: "这人不想打酱油",
+        description: "一个不想打酱油却又一直在打酱油的人~",
         medias: {
             Github: "https://github.com/bbruceyuan",
             BiliBili: "https://space.bilibili.com/12420432",
@@ -31,15 +34,26 @@ export default hopeTheme({
         },
     },
     plugins: {
+        seo: {},
+        comment: {
+            provider: "Giscus",
+            repo: "bbruceyuan/bbruceyuan.github.io",
+            repoId: "MDEwOlJlcG9zaXRvcnkyNDU3Njc0NzU=",
+            category: "Announcements",
+            categoryId: "DIC_kwDODqYdM84Cbg9U",
+        },
         blog: {
             excerptLength: 0,
             excerpt: false,
         },
         mdEnhance: {
           // 使用 KaTeX 启用 TeX 支持
-          katex: true,
+        //   katex: true,
+          mathjax: true,
           footnote: true,
           tasklist: true,
+          imgLazyload: true,
         },
+        
       },
 })
