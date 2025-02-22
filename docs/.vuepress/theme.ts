ede1fbdd78d31c68bb113652faa99f69f5a571d4
Chaofa Yuan <bruceyuan123@gmail.com>
2025-02-22 06:15:20 +0000

    update author name

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -8,7 +8,7 @@ export default hopeTheme({
   hostname: "https://bruceyuan.com",
   favicon: "/img/icon.webp",
   author: {
-    name: "chaofa",
+    name: "Chaofa Yuan",
     url: "https://bruceyuan.com",
   },
 
